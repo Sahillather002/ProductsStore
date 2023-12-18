64892d083576173af2249ab6e2cb4c19f8084b37
SahilGrip <sahillather002@gmail.com>
2023-12-18 20:16:31 +0530

    missing import added

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import '@/styles/globals.css'
 import { ThemeProvider } from 'next-themes'
-
+import Head from 'next/head'
 export default function App({ Component, pageProps }) {
   return (
     <>
